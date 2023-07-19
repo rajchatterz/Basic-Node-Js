dbdf18d66b0695070c3f2f2fde0f395e35aafc99
rajchatterz <iamraj.space@gmail.com>
2023-07-19 17:40:12 +0530

    done with basic file structure

---
+++ app.js
@@ -0,0 +1,5 @@
+// console.log(a)
+import a from './index'
+// const app = require('./index')
+app.avg(10,20)
+app.percent(100,200)
--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const fs = require('fs')
+// Async way
+// because it is an asynchrounous function
+fs.readFile('./raj.txt','utf-8',(err,data)=>{
+    if(err){
+        return err
+    }
+    console.log(data)
+})
 
-console.log("hey")
+console.log("I am the first")
+
+// lets make it a async sync function
+
+const a = fs.readFileSync('./raj.txt','utf-8')
+console.log(a)
+console.log("I am first")
+
+let b = "raj Chatterjee"
+fs.writeFileSync('./raj2.txt',b)
+
+const file = fs.readFileSync('./raj2.txt','utf-8')
+console.log(file)
